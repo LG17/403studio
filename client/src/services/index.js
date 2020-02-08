90da3ff819eea9eb27686c19889090e89774cc66
LG17 <jingyilg@sina.com>
2020-02-08 17:20:24 +0800

    添加nprogress，请求拦截器

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,20 @@
-const axios = require('axios')
+import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const request = axios.create()
+NProgress.configure({ showSpinner: false })
+
+request.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+request.interceptors.response.use(response => {
+  NProgress.done()
+  return response
+}, function (error) {
+  NProgress.done()
+  return Promise.reject(error)
+})
 
 export default request
